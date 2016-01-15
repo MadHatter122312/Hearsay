ef0b1390d584f7b23c89b37dda44e87924f0eb1b
Malcolm Macrae-Gibson <[A[Dcapitalkind>
2016-01-15 13:18:23 -0500

    adds partials to index.ejs

--- middleware/loadUser.js
+++ middleware/loadUser.js
@@ -4,15 +4,16 @@ var User = require('../models/user');
 // ~~~~~~~~~~CUSTOM MIDDLEWARE~~~~~~~~~~~~~ //
 // If token is found, load that user
 function loadUser(req, res, next){
-  if(req.cookies.token)
+  if(req.cookies.token){
     // Find that user by the token
     User.findOne({token: req.cookies.token}, function(err, databaseUser){
       if(err) return err;
       req.user = databaseUser;
       next();
     });
-  else
+  } else {
     next();
+  }
 };
 
 // ~~~~~~~~~~EXPORTS~~~~~~~~~~~~~ //
