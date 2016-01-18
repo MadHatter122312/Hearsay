3a25a10c03c4928745d5317703c5a5790d026ce9
Malcolm Macrae-Gibson <[A[Dcapitalkind>
2016-01-18 16:27:26 -0500

    working on delete button

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -103,7 +103,7 @@ function renderHearsay(hearsay){
   var $el = $('<div>').addClass('hearsay content-block');
   $el.append( $('<h2>').addClass('username').text(hearsay.username) ); //again, this will go away but is left in for testing purposes
   $el.append( $('<p>').addClass('body').text(hearsay.body) );
-  $el.append( $('<button>').addClass('btn btn-default').attr('id', 'delete-hearsay').data('id', hearsay._id).text('Delete') );
+  $el.append( $('<button>').addClass('btn btn-default').attr('id', 'delete-hearsay').attr('data-toggle', 'modal').data('id', hearsay._id).text('Delete') );
 
   var $commentList = $('<section>').addClass('comment-list');
   for (var i = 0; i < hearsay.comment.length; i++) {
@@ -192,18 +192,20 @@ function updateHearsaysAndViews(){
 
 // ~~~~~~~~~~~~~~~~~~~~ DELETE ~~~~~~~~~~~~~~~~~~~~ //
 function removeHearsay(payload){
-  $('body').on('click', '#delete-hearsay', function(e){
-    e.preventDefault();
-    var hearsay = $(this);
-    $.ajax({
-      method: 'delete',
-      url: '/api/hearsays/' + hearsay.data('id'),
-      success: function(){
-        $(this).remove();
-        updateHearsaysAndViews();
-      }
-    });
-  });
+ $('body').on('click', '#delete-hearsay', function(e){
+   e.preventDefault();
+   var hearsay = $(this);
+   var hearsayUserName = $(this).username
+   console.log(hearsayUserName);
+   $.ajax({
+     method: 'delete',
+     url: '/api/hearsays/' + hearsay.data('id'),
+     success: function(){
+       $(this).remove();
+       updateHearsaysAndViews();
+     }
+   });
+ });
 }
 
 // ~~~~~~~~~~~~~~~~~~~~ SET FORMS ~~~~~~~~~~~~~~~~~~~~ //
