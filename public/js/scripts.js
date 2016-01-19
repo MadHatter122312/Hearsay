82eee01a3ed8a1254fe210c08cf56d1d95041e29
David Lee <ddlee.907@gmail.com>
2016-01-19 18:44:28 -0500

    delete render comments

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -130,23 +130,6 @@ Handlebars.registerHelper('toHuman', function(date){
 });
 
 
-// Render Hearsays
-function renderHearsay(hearsay){
-}
-
-// Render Hearsay list
-function renderHearsayList(hearsays, $list){
-
-}
-
-// Render the Comment Form
-function renderCommentForm(hearsay){
-}
-
-// Render Comments
-function renderComment(comment){
-}
-
 // ~~~~~~~~~~~~~~~~~~~~ UPDATE ~~~~~~~~~~~~~~~~~~~~ //
 // Send request to update a User
 function updateUser(userData, callback){
